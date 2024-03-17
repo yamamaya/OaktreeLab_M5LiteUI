e14ec097ae4d04793ba1a494e1c07f50122f8b4b
Mao Morimoto <yneko@yamamaya.com>
2024-03-17 14:54:05 +0900

    Remove unnecessary debug messages

--- UIScreen.cpp
+++ UIScreen.cpp
@@ -4,8 +4,4 @@
 using namespace OaktreeLab::M5LiteUI;
 
 OaktreeLab::M5LiteUI::UIScreen::UIScreen() : UIWindow( base->getScreenRectangle() ) {
-  Rectangle rect = getClientRectangle();
-  char buff[100];
-  sprintf( buff, "UIScreen( %d, %d, %d, %d )", rect.left, rect.top, rect.width, rect.height );
-  Serial.println( buff );
 }
